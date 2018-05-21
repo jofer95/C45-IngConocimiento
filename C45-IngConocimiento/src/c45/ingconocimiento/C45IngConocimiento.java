1be5091a121839e4bc3be3ea0e84c51373ebbc1f
Jorge Fernando Barraza Zambada <jofer_bz20@hotmail.com>
2018-05-21 13:45:12 -0600

    Commit inicial del proyecto
    Se creo el proyecto.

---
+++ C45-IngConocimiento/src/c45/ingconocimiento/C45IngConocimiento.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c45.ingconocimiento;
+
+/**
+ *
+ * @author jorgebarraza
+ */
+public class C45IngConocimiento {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
